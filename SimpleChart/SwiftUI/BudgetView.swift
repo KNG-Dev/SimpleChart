038cb9758d8af3b44d3199490a1afa99c9648753
Kenny Ho <ken.ho1996@hotmail.com>
2020-08-23 20:12:55 -0400

    Toggle Dark mode in Menu

--- SimpleChart/SwiftUI/BudgetView.swift
+++ SimpleChart/SwiftUI/BudgetView.swift
@@ -13,50 +13,71 @@ let screen = UIScreen.main.bounds
 struct BudgetView: View {
     var store = SubscriptionViewModel()
     @State var showPresent = false
+    @State var showMenu = false
+    @State var dark = false
     
     //Custom Initializers
     init() {
         setupNavBar()
+        UITableView.appearance().backgroundColor = UIColor(named: "Background")
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                VStack {
-                    HStack {
-                        Text("Overview")
-                            .foregroundColor(Color("TextColor1"))
-                        Spacer()
-                        Text("•••")
-                            .foregroundColor(Color(.lightGray))
+        ZStack(alignment: .leading) {
+            NavigationView {
+                List {
+                    VStack {
+                        HStack {
+                            Text("Overview")
+                                .foregroundColor(Color("TextColor1"))
+                            Spacer()
+                            Text("•••")
+                                .foregroundColor(Color(.lightGray))
+                        }
+                        .padding()
+                        
+                        ChartViewRepresentable()
+                            .frame(width: screen.width, height: 300)
+                        
                     }
-                    .padding()
-                    
-                    ChartViewRepresentable()
-                        .frame(width: screen.width, height: 300)
-                    
-                }
-                .background(Color("Background"))
-                .listRowInsets(EdgeInsets())
-                
-                SubscriptionsSection(store: self.store)
                     .background(Color("Background"))
-            }
-                
-            .navigationBarTitle("Budget", displayMode: .inline)
-            .navigationBarItems(leading:
-                NavigationLink(destination: Text("ThirdView")) {
-                    Image(systemName: "line.horizontal.3")
-                        .resizable()
-                        .frame(width: 16, height: 12)
+                    .listRowInsets(EdgeInsets())
                     
-                }, trailing:
-                
-                Button(action: {self.addUpdate()}) {
-                    Image(systemName: "plus")
-                        .resizable()
-                        .frame(width: 14, height: 14)
-            })
+                    SubscriptionsSection(store: self.store)
+                        .background(Color("Background"))
+                }
+                    
+                .navigationBarTitle("Budget", displayMode: .inline)
+                .navigationBarItems(leading:
+                    Button(action: {self.showMenu.toggle()}) {
+                        
+                        Image(systemName: "line.horizontal.3")
+                            .resizable()
+                            .frame(width: 16, height: 12)
+                        
+                    }, trailing:
+                    
+                    Button(action: {self.addUpdate()}) {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 14, height: 14)
+                })
+            }
+            
+            Color(.black).opacity(0.3)
+                .edgesIgnoringSafeArea(.all)
+                .opacity(self.showMenu ? 1 : 0)
+                .animation(.easeIn(duration: 0.3))
+                .onTapGesture {
+                    self.showMenu.toggle()
+            }
+            
+            MenuView()
+                .frame(width: screen.width / 1.5)
+                .overlay(Rectangle().stroke(Color.primary.opacity(0.2), lineWidth: 2).shadow(radius: 3).edgesIgnoringSafeArea(.all))
+                .offset(x: self.showMenu ? 0 : -screen.width / 1.5)
+                .animation(.easeOut(duration: 0.3))
+            
         }
     }
     
@@ -127,6 +148,7 @@ struct ChartViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: ChartView, context: Context) {
+        
     }
 }
 
--- SimpleChart/SwiftUI/MenuView.swift
+++ SimpleChart/SwiftUI/MenuView.swift
@@ -9,20 +9,68 @@
 import SwiftUI
 
 struct MenuView: View {
-    init() {
-//        UITableView.appearance().separatorStyle = .none
-    }
+    @State var dark = false
+    
+    
     
     var body: some View {
-        List(menu) {item in
-            HStack(spacing: 12) {
-                Image(systemName: item.image)
-                Text(item.title)
-            }
+        List {
+            
+            
+            HStack {
+                Image(systemName: "person.circle")
+                Text("Dark Mode")
+                Spacer()
                 
+                Button(action: { self.toggleDarkMode() }) {
+                    Text("Click Me")
+                        .foregroundColor(Color.blue)
+                }
+            }
             
             .frame(height: 50)
+ 
+            ForEach(accountMenu) { item in
+                HStack(spacing: 12) {
+                    Image(systemName: item.image)
+                    Text(item.title)
+                }
+                .frame(height: 50)
+            }
+            .listRowBackground(Color("SlideMenuColor"))
+            
+            Rectangle()
+                .foregroundColor(Color(.clear))
+                .frame(height: 1)
+                
+            
+            ForEach(settingMenu) { item in
+                HStack(spacing: 12) {
+                    Image(systemName: item.image)
+                    Text(item.title)
+                }
+                .frame(height: 50)
+            }
+            
+            .listRowBackground(Color("SlideMenuColor"))
+            
         }
+        
+        .environment(\.defaultMinListRowHeight, 1)
+            
+        .onAppear(perform: {
+            UITableView.appearance().separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            UITableView.appearance().backgroundColor = UIColor(named: "SlideMenuColor")
+            
+        })
+        
+        
+        
+    }
+    
+    func toggleDarkMode() {
+        dark.toggle()
+        UIApplication.shared.windows.first?.rootViewController?.view.overrideUserInterfaceStyle = self.dark ? .dark : .light
     }
 }
 
@@ -32,11 +80,14 @@ struct Menu: Identifiable {
     let image: String
 }
 
-let menu = [
+let accountMenu = [
     Menu(title: "Activity", image: "person.circle"),
     Menu(title: "Expenses", image: "calendar"),
     Menu(title: "Goals", image: "person.circle"),
     Menu(title: "Certificate of Deposit", image: "dollarsign.circle"),
+]
+
+let settingMenu = [
     Menu(title: "Support", image: "message"),
     Menu(title: "Personal Info", image: "person.circle"),
     Menu(title: "Account Details", image: "house"),
@@ -46,5 +97,6 @@ let menu = [
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
+        .environment(\.colorScheme, .dark)
     }
 }
